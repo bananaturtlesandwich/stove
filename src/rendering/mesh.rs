09db3dc84f29c769d0be4337777c744742f208e2
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-02-05 16:00:11 +0000

    wireframe for clarity

--- src/rendering/mesh.rs
+++ src/rendering/mesh.rs
@@ -1,17 +1,31 @@
 use miniquad::*;
 
 pub struct Mesh {
-    pub pipeline: Pipeline,
-    pub bindings: Bindings,
     len: i32,
+    solid_pipeline: Pipeline,
+    solid_bindings: Bindings,
+    wire_pipeline: Pipeline,
+    wire_bindings: Bindings,
 }
 
 impl Mesh {
     pub fn new(ctx: &mut Context, vertices: Vec<glam::Vec3>, indices: Vec<u32>) -> Self {
-        let shader = Shader::new(
+        let solid = Shader::new(
             ctx,
             include_str!("mesh.vert"),
-            include_str!("mesh.frag"),
+            include_str!("solid.frag"),
+            ShaderMeta {
+                uniforms: UniformBlockLayout {
+                    uniforms: vec![UniformDesc::new("vp", UniformType::Mat4)],
+                },
+                images: vec![],
+            },
+        )
+        .unwrap();
+        let wire = Shader::new(
+            ctx,
+            include_str!("mesh.vert"),
+            include_str!("wire.frag"),
             ShaderMeta {
                 uniforms: UniformBlockLayout {
                     uniforms: vec![UniformDesc::new("vp", UniformType::Mat4)],
@@ -22,11 +36,12 @@ impl Mesh {
         .unwrap();
         let len = vertices.len() as i32;
         Self {
-            pipeline: Pipeline::with_params(
+            len,
+            solid_pipeline: Pipeline::with_params(
                 ctx,
                 &[BufferLayout::default()],
                 &[VertexAttribute::with_buffer("pos", VertexFormat::Float3, 0)],
-                shader,
+                solid,
                 PipelineParams {
                     depth_test: Comparison::LessOrEqual,
                     depth_write: true,
@@ -34,21 +49,45 @@ impl Mesh {
                     ..Default::default()
                 },
             ),
-            bindings: Bindings {
+            solid_bindings: Bindings {
                 vertex_buffers: vec![Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices)],
                 index_buffer: Buffer::immutable(ctx, BufferType::IndexBuffer, &indices),
                 images: vec![],
             },
-            len,
+            wire_pipeline: Pipeline::with_params(
+                ctx,
+                &[BufferLayout::default()],
+                &[VertexAttribute::with_buffer("pos", VertexFormat::Float3, 0)],
+                wire,
+                PipelineParams {
+                    depth_test: Comparison::LessOrEqual,
+                    depth_write: true,
+                    primitive_type: PrimitiveType::Lines,
+                    ..Default::default()
+                },
+            ),
+            wire_bindings: Bindings {
+                vertex_buffers: vec![Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices)],
+                index_buffer: Buffer::immutable(
+                    ctx,
+                    BufferType::IndexBuffer,
+                    &indices
+                        .chunks_exact(3)
+                        .flat_map(|i| [i[0], i[1], i[0], i[2], i[1], i[2]])
+                        .collect::<Vec<_>>(),
+                ),
+                images: vec![],
+            },
         }
     }
 
     pub fn draw(&self, ctx: &mut Context, uniform: &glam::Mat4) {
-        unsafe {
-            gl::glEnable(gl::GL_PROGRAM_POINT_SIZE);
-        }
-        ctx.apply_pipeline(&self.pipeline);
-        ctx.apply_bindings(&self.bindings);
+        ctx.apply_pipeline(&self.solid_pipeline);
+        ctx.apply_bindings(&self.solid_bindings);
+        ctx.apply_uniforms(uniform);
+        ctx.draw(0, self.len, 1);
+        ctx.apply_pipeline(&self.wire_pipeline);
+        ctx.apply_bindings(&self.wire_bindings);
         ctx.apply_uniforms(uniform);
         ctx.draw(0, self.len, 1);
     }
--- src/rendering/mesh.vert
+++ src/rendering/mesh.vert
@@ -5,6 +5,5 @@ in vec3 pos;
 uniform mat4 vp;
 
 void main() {
-    gl_PointSize = 3;
     gl_Position = vp * vec4(pos, 1.0);
 }
