d440bad7f53be2683b97a4abae97668835e005a0
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-08-30 15:12:01 +0100

    resolve imports in actorcomponent extra data

--- src/actor/transplant.rs
+++ src/actor/transplant.rs
@@ -42,49 +42,61 @@ impl super::Actor {
         // resolve all import references from exports
         let import_offset = recipient.imports.len() as i32;
         let mut imports = Vec::new();
-        for child in children.iter_mut() {
-            on_import_refs(child, |index| {
-                let Some(import) = donor.get_import(*index) else {
-                    return;
-                };
-                index.index = match recipient.find_import_no_index(
-                    &import.class_package,
-                    &import.class_name,
-                    &import.object_name,
-                ) {
-                    // sometimes e.g for GEN_VARIABLEs you want those imports
-                    Some(existing)
-                        if donor.get_import(import.outer_index).is_some_and(|imp| {
-                            recipient
-                                .get_import(PackageIndex::new(existing))
-                                .and_then(|existing| recipient.get_import(existing.outer_index))
-                                .is_some_and(|import| {
-                                    imp.class_package.eq_content(&import.class_package)
-                                        && imp.class_name.eq_content(&import.class_name)
-                                        && imp.object_name.eq_content(&import.object_name)
-                                })
-                        }) =>
-                    {
-                        existing
-                    }
-                    _ => {
-                        -import_offset
-                            - match imports.iter().position(|imp: &Import| {
+        let mut resolve = |index: &mut PackageIndex| {
+            let Some(import) = donor.get_import(*index) else {
+                return;
+            };
+            index.index = match recipient.find_import_no_index(
+                &import.class_package,
+                &import.class_name,
+                &import.object_name,
+            ) {
+                // sometimes e.g for GEN_VARIABLEs you want those imports
+                Some(existing)
+                    if donor.get_import(import.outer_index).is_some_and(|imp| {
+                        recipient
+                            .get_import(PackageIndex::new(existing))
+                            .and_then(|existing| recipient.get_import(existing.outer_index))
+                            .is_some_and(|import| {
                                 imp.class_package.eq_content(&import.class_package)
                                     && imp.class_name.eq_content(&import.class_name)
                                     && imp.object_name.eq_content(&import.object_name)
-                            }) {
-                                Some(existing) => existing + 1,
-                                None => {
-                                    imports.push(import.clone());
-                                    // this actually pads perfectly so no need for + 1
-                                    imports.len()
-                                }
-                            } as i32
-                    }
+                            })
+                    }) =>
+                {
+                    existing
                 }
-            })
+                _ => {
+                    -import_offset
+                        - match imports.iter().position(|imp: &Import| {
+                            imp.class_package.eq_content(&import.class_package)
+                                && imp.class_name.eq_content(&import.class_name)
+                                && imp.object_name.eq_content(&import.object_name)
+                        }) {
+                            Some(existing) => existing + 1,
+                            None => {
+                                imports.push(import.clone());
+                                // this actually pads perfectly so no need for + 1
+                                imports.len()
+                            }
+                        } as i32
+                }
+            }
+        };
+        for child in children.iter_mut() {
+            on_import_refs(child, &mut resolve)
         }
+        if donor.get_engine_version() >= unreal_asset::engine_version::EngineVersion::VER_UE5_1 {
+            for child in &mut children[1..] {
+                on_extra_import_refs(
+                    donor.get_name_map(),
+                    recipient.get_name_map(),
+                    child,
+                    &mut resolve,
+                )
+            }
+        }
+
         for prop in children
             .iter_mut()
             .filter_map(ExportNormalTrait::get_normal_export_mut)
@@ -147,10 +159,10 @@ impl super::Actor {
 }
 
 /// on all of an export's possible references to imports
-fn on_import_refs(export: &mut Export, mut func: impl FnMut(&mut PackageIndex)) {
+fn on_import_refs(export: &mut Export, func: &mut impl FnMut(&mut PackageIndex)) {
     if let Some(norm) = export.get_normal_export_mut() {
         for prop in norm.properties.iter_mut() {
-            super::on_prop_refs(prop, &mut func);
+            super::on_prop_refs(prop, func);
         }
     }
     let export = export.get_base_export_mut();
@@ -160,9 +172,36 @@ fn on_import_refs(export: &mut Export, mut func: impl FnMut(&mut PackageIndex)) 
     export
         .serialization_before_create_dependencies
         .iter_mut()
-        .for_each(&mut func);
+        .for_each(&mut *func);
     export
         .create_before_serialization_dependencies
         .iter_mut()
-        .for_each(&mut func);
+        .for_each(func);
+}
+
+fn on_extra_import_refs(
+    donor: unreal_asset::containers::SharedResource<unreal_asset::containers::NameMap>,
+    mut recipient: unreal_asset::containers::SharedResource<unreal_asset::containers::NameMap>,
+    export: &mut Export,
+    func: &mut impl FnMut(&mut PackageIndex),
+) {
+    use byteorder::{ReadBytesExt, WriteBytesExt, LE};
+    use std::io::Seek;
+    let Some(norm) = export.get_normal_export_mut() else {
+        return;
+    };
+    let mut data = std::io::Cursor::new(&mut norm.extras);
+    let _ = data.read_i32::<LE>();
+    for _ in 0..data.read_u32::<LE>().unwrap() {
+        let mut index = PackageIndex::new(data.read_i32::<LE>().unwrap());
+        func(&mut index);
+        let _ = data.seek(std::io::SeekFrom::Current(-4));
+        let _ = data.write_i32::<LE>(index.index);
+        let name = donor
+            .get_ref()
+            .get_name_reference(data.read_i32::<LE>().unwrap())
+            .to_string();
+        let _ = data.seek(std::io::SeekFrom::Current(-4));
+        let _ = data.write_i32::<LE>(recipient.get_mut().add_name_reference(name, false));
+    }
 }
