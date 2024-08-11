9a7d7c7981894e5cdc5f28ca806dae866b60d57e
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2024-08-11 14:59:56 +0100

    persist fullscreen

--- src/persistence.rs
+++ src/persistence.rs
@@ -4,12 +4,14 @@ pub fn load(
     mut commands: Commands,
     mut ctx: bevy_egui::EguiContexts,
     mut fps: ResMut<bevy_framepace::FramepaceSettings>,
+    mut windows: Query<&mut Window>,
 ) {
     let mut appdata = AppData {
         textures: true,
         rate: 60.0,
         ..default()
     };
+    let mut fullscreen = false;
     ctx.ctx_mut().memory_mut(|storage| {
         if let Some(config) = config()
             .map(|config| config.join("config.ron"))
@@ -36,15 +38,20 @@ pub fn load(
         retrieve(&mut appdata.script, "SCRIPT", data);
         retrieve(&mut appdata.cap, "CAP", data);
         retrieve(&mut appdata.rate, "RATE", data);
+        retrieve(&mut fullscreen, "FULLSCREEN", data);
     });
     fps.limiter = match appdata.cap {
         true => bevy_framepace::Limiter::from_framerate(appdata.rate),
         false => bevy_framepace::Limiter::Off,
     };
+    if fullscreen {
+        let mut window = windows.single_mut();
+        window.mode = bevy::window::WindowMode::BorderlessFullscreen
+    }
     commands.insert_resource(appdata);
 }
 
-pub fn write(mut ctx: bevy_egui::EguiContexts, appdata: Res<AppData>) {
+pub fn write(mut ctx: bevy_egui::EguiContexts, appdata: Res<AppData>, windows: Query<&Window>) {
     if !appdata.is_changed() {
         return;
     }
@@ -59,6 +66,13 @@ pub fn write(mut ctx: bevy_egui::EguiContexts, appdata: Res<AppData>) {
         storage.insert_persisted(Id::new("SCRIPT"), appdata.script.clone());
         storage.insert_persisted(Id::new("CAP"), appdata.cap);
         storage.insert_persisted(Id::new("RATE"), appdata.rate);
+        storage.insert_persisted(
+            Id::new("FULLSCREEN"),
+            windows
+                .get_single()
+                .map(|window| window.mode != bevy::window::WindowMode::Windowed)
+                .unwrap_or_default(),
+        );
         if let Some(config) = config() {
             let _ = std::fs::create_dir_all(&config);
             if let Ok(data) = ron::to_string(&storage) {
